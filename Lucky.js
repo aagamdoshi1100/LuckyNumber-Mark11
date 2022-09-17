04cfaa26c68da9dffdb25308e22965f6fe269c07
aagamdoshi1100 <108725133+aagamdoshi1100@users.noreply.github.com>
2022-09-17 17:32:33 +0530

    feat : added date checker

--- Lucky.js
+++ Lucky.js
@@ -17,6 +17,16 @@ checkButton.addEventListener("click", function GetDateByUser(){
       //  console.log(StringToNumber);
         sum += StringToNumber;
         ParaG.innerText = sum;
-    }
 
+    }
+        Checker(sum,LuckyNo.value);
 });
+
+function Checker(a,b){
+    if (a%b === 0){
+         ParaG.innerText = "Your birth date is lucky";
+
+    }else{
+        ParaG.innerText = "Your birth date is not lucky";
+    }
+}

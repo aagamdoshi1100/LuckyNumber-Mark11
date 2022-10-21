55df03d1e3bc56e7dec034c52795f32b1eb433c6
Aagam Doshi <aagamdoshi1100@gmail.com>
2022-10-21 13:54:01 +0530

    feat: added negative value handler

--- Lucky.js
+++ Lucky.js
@@ -1,32 +1,54 @@
 var birthDay = document.querySelector("#birthday-date");
 var LuckyNo = document.querySelector("#Lucky-Number");
+var LuckyNoValidkar = document.querySelector("#validkar");
 var checkButton = document.querySelector("#check");
 var sum=0;
 var ParaG=document.querySelector("#para");
 
-checkButton.addEventListener("click", function GetDateByUser(){
+checkButton.addEventListener("click", NegativeValueHandler);
+
+
+function GetDateByUser(){
   ///  console.log(birthDay.value);
     var DOB = birthDay.value;
     var DOB = DOB.replaceAll("-","");
 
-  //  console.log(DOB);
+    console.log(typeof(DOB));
 
     for(i=0; i<DOB.length; i++){
 
         var StringToNumber =parseInt(DOB.charAt(i));
-      //  console.log(StringToNumber);
+        console.log(StringToNumber);
         sum += StringToNumber;
-        ParaG.innerText = sum;
+        console.log(sum);
+       // ParaG.innerText = sum;
 
     }
         Checker(sum,LuckyNo.value);
-});
+}
 
 function Checker(a,b){
     if (a%b === 0){
+        ParaG.style.color="Green";
          ParaG.innerText = "Your birth date is lucky";
 
     }else{
+      ParaG.style.color="Red";
         ParaG.innerText = "Your birth date is not lucky";
     }
+}
+
+
+function NegativeValueHandler(){
+
+  if(LuckyNo.value<=0){
+    LuckyNoValidkar.style.color="Red";
+    LuckyNoValidkar.style.display = "block";
+    LuckyNoValidkar.innerText = "Please enter valid Lucky number. Value must be positive";
+  }else{
+    
+    LuckyNoValidkar.style.display = "none";
+    GetDateByUser();
+  }
+
 }
